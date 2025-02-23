f418ebf1511ba45b86af8e68a1dbb7efb6727052
roee-golan-42 <roee.golan42@gmail.com>
2025-02-23 17:06:14 +0200

    whatsapp poc

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,27 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://web.whatsapp.com/');
+  // 
+  // console.log(await page.locator(`xpath=//*[contains(text(), 'Log in with phone number')]`).innerText());
+  // await page.waitForTimeout(15000)
+  await page.locator(`//*[contains(text(), 'Log in with phone number')]`).waitFor({ state: 'visible' });
+  await page.locator(`//*[contains(text(), 'Log in with phone number')]`).hover();
+  await page.locator(`//*[contains(text(), 'Log in with phone number')]`).click();
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.locator(`//*[contains(text(), 'Log in with phone number')]`).click({ force: true });
+
+  await page.locator(`[aria-label='Type your phone number.']`).fill("")
+  await page.locator(`[aria-label='Type your phone number.']`).fill("+972586640882")
+  await page.click("//button//*[contains(text(), 'Next')]")
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
