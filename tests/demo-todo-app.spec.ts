96e606f7aeedea70950d8709e1b3c0f3500a33f4
roeegolan <roee.golan42@gmail.com>
2025-02-24 10:35:58 +0200

    .

---
+++ tests/demo-todo-app.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://web.whatsapp.com/");
+
+  await page
+    .locator(`//*[contains(text(), 'Log in with phone number')]`)
+    .waitFor({ state: "visible" });
+  await page
+    .locator(`//*[contains(text(), 'Log in with phone number')]`)
+    .hover();
+  await page
+    .locator(`//*[contains(text(), 'Log in with phone number')]`)
+    .click();
+
+  // await page.locator(`[aria-label='Type your phone number.']`).fill("")
+  await page
+    .locator(`[aria-label='Type your phone number.']`)
+    .fill("+972586640882");
+
+  await page.click("//button//*[contains(text(), 'Next')]");
+
+  await page.waitForTimeout(60000);
+});
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
